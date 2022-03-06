abcc1c23549ecc88e95b11edc2bd8a599f724523
Okan Çilesiz <56220113+okancilesiz@users.noreply.github.com>
2022-03-06 15:52:27 +0300

    Add files via upload

---
+++ 27022022.py
@@ -0,0 +1,17 @@
+"""
+Burası
+Açıklama
+Satırlarıdır
+"""
+print("Merhaba Pyhton!!") #Burası tek satırlık açıklama satırı.
+print("Benim adım Okan") #ctrl+d satı çoklar, ctrl+x satır siler
+print("Benim adım Okan") #ctrl+d satır çoklar, ctrl x satır siler
+print("Benim adım Okan") #ctrl+d satır çoklar, ctrl x satır siler
+print("Benim adım Okan") #ctrl+d satır çoklar, ctrl x satır siler
+print("Benim adım Okan") #ctrl+d satır çoklar, ctrl x satır siler
+print("Benim Adım Okan" , "Btk Akademi" , "123")
+print("Benim Adım Okan" + "Btk Akademi" + "123")
+print("CTRL D SATIR KOPYA CTRL X SATIR SİLME") #ctrl+d satır çoklar
+print(123+456)
+print("123"+"456")
+
