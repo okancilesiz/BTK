4c13f9e37ac0b51c9e742f007dc4d41ca9846626
Okan Çilesiz <56220113+okancilesiz@users.noreply.github.com>
2022-03-06 15:54:23 +0300

    Add files via upload

---
+++ Ders1/hesapmakinesi.py
@@ -0,0 +1,12 @@
+print("""HESAP MAKİNESİ
+1- TOPLA
+2- ÇIKAR
+3- ÇARP
+4- BÖL
+5- ÇIK""")
+islem=input("İşleminiz")
+sayi1=int(input("1. Sayıyı Giriniz"))
+sayi2=int(input("2. Sayıyı Giriniz"))
+print("Sayılarınızın Toplamı : ",sayi1+sayi2)
+
+
