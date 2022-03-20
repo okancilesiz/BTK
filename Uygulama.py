8e45c9135fe60e26b3aa42b7b4bb0a7a3d4c4b0d
Okan Çilesiz <56220113+okancilesiz@users.noreply.github.com>
2022-03-20 15:51:23 +0300

    Delete Uygulama.py

--- Uygulama.py
+++
@@ -1,9 +0,0 @@
-# Kullanıcıya sürekli ismi sorulsun q ya basınca çıksın
-# while True:
-#     isim = input("Lütfen isminizi giriniz")
-#     if isim =="q":
-#         break
-
-isim=""
-while isim!="q":
-    isim=input("İsminiz:")
