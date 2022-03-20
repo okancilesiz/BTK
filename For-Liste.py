6794d05650d8930f5225a14420b8b1d56e0e27b3
Okan Çilesiz <56220113+okancilesiz@users.noreply.github.com>
2022-03-20 15:51:17 +0300

    Delete For-Liste.py

--- For-Liste.py
+++
@@ -1,7 +0,0 @@
-
-aylar=["Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık"]
-# for i in range(len(aylar)):
-#     print(aylar[i])
-for i in aylar: # For döngüsü ile listeler üzerinde de işlem yapılabilir
-    print(i)
-
